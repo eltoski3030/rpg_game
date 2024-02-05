8eb38245f68b7233b0ca14c7aa121e3207612e19
Pablolizardi <pabloagra3018@gmail.com>
2024-02-04 23:20:44 -0400

    funcionalites

--- logInScript.js
+++ logInScript.js
@@ -1,19 +1,28 @@
+
+
+// accprompt.addEventListener('click', accprompt.style = "block");
+
+
+
+
+
 const logInButton = document.getElementById('log-in-button');
 const accountButton = document.getElementById('acc-button');
 const username = document.getElementById('user');
 const password = document.getElementById('password');
+const accprompt = document.getElementById("acc-promp");
+
+accprompt.style.display = "none";
 
 function authentication() {
-    
     const usernameValue = username.value;
     const passwordValue = password.value;
 
     if (passwordValue === '' || usernameValue === '') {
         alert('Password and username are missing');
     } else {
-        
         window.location.href = 'index.html';
-        
     }
 }
+
 logInButton.addEventListener('click', authentication);
